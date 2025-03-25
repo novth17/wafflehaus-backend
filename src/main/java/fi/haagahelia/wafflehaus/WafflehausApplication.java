ac40650c165820ba5abb92700515472f8b6b1a57
Hien Nguyen <hien.nguyen2@myy.haaga-helia.fi>
2025-03-25 20:05:17 +0200

    Initial commit

---
+++ src/main/java/fi/haagahelia/wafflehaus/WafflehausApplication.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.wafflehaus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WafflehausApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WafflehausApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/haagahelia/wafflehaus/WafflehausApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.wafflehaus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WafflehausApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
